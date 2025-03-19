26214d432149f05523afa52935116fee06fc56ca
aleesha-00 <aleeshap4021@gmail.com>
2025-03-19 13:59:23 +0530

    postgresql backend connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
@@ -5,10 +7,26 @@ import 'package:propconnect/create_account_screen.dart';
 import 'package:propconnect/splash_screen.dart';
 
 void main() {
+  checkBackendConnection(); // Call the function before running the app
+
   runApp(DevicePreview(
     enabled: !kReleaseMode,
-    builder: (context) =>const  MyApp(),
+    builder: (context) => const MyApp(),
   ));
+}
+
+void checkBackendConnection() async {
+  try {
+    final response = await http.get(Uri.parse('http://127.0.0.1:5000/'));
+
+    if (response.statusCode == 200) {
+      print("✅ Flask Backend Connected: ${jsonDecode(response.body)}");
+    } else {
+      print("❌ Failed to connect to Flask: Status Code ${response.statusCode}");
+    }
+  } catch (e) {
+    print("❌ Error connecting to Flask: $e");
+  }
 }
 
 class MyApp extends StatelessWidget {
